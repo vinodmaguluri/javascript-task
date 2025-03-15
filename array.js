3c6b696a521286f0b1d894f45e6d83824bd5dd68
vinodmaguluri <gnanendramaguluri3@gmail.com>
2025-03-15 22:17:28 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,54 @@
+//1st question
+var arr=[1,2,3,4,5]
+console.log(arr);
+
+//2nd question
+var arr=[1,2,3,4,5]
+console.log(arr[2]);
+
+//3rd question
+var arr=[1,2,3,4,5]
+arr[1]=7
+console.log(arr[1]);
+
+//4th question
+function sun(){
+ var arr=[1,2,3,4,5,]
+ return arr.length
+}
+console.log(sun())
+
+//5th question
+var arr=[1,2,3,4,5]
+arr[5]=6;
+console.log(arr);
+
+//6th question
+var arr=[1,2,3,4,5]
+arr.pop();
+console.log(arr);
+
+//7th question
+var arr=[1,2,3,4,5]
+for(i=1;i<=5;i++){
+    console.log(arr[i]);
+}
+
+//8th question
+var arr=[1,2,3,4,5]
+b=6
+for(i=0;i<arr.length;i++){
+  if(b==arr[i]){
+    console.log('yes it is prasent in the array')
+  }else
+  console.log('no')
+}
+
+//9th question
+var arr=[1,2,3,4,5]
+var a=arr;
+console.log(a);
+
+//10th question
+var arr=[1,2,3,4,5]
+console.log(arr.join())
